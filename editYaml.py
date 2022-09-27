a70df1e7744c44261e3a35c73dc9b2ed9b4fa70a
Schattauer <markus.schattauer@fujitsu.com>
2022-09-28 00:01:32 +0200

    initial

---
+++ editYaml.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+
+if __name__ == '__main__':
+    newImage=sys.argv[1]
+
+    newfile=""
+
+    with open("./overlays/prod/appdeploy.yml","r") as f:
+        list_doc = f.readlines()
+        for line in list_doc:
+            wl=line
+            if "image:" in wl:
+                h=wl.split(":")
+                wl=h[0]+ ": " +newImage + "\n"
+            newfile=newfile+wl
+
+    with open("./overlays/prod/appdeploy.yml","w") as f:
+        f.write(newfile)
